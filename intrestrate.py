ee7f0e72aa5ba8ee7eab572c1d1aa2cee175b39e
Harpreetsingh21 <133197372+Harpreetsingh21@users.noreply.github.com>
2023-05-30 20:26:30 +0530

    Add files via upload

---
+++ intrestrate.py
@@ -0,0 +1,18 @@
+p=int(input("Enter the principal amount :"))
+t=int(input("Enter the time period :"))
+g=input("Enter your gender : ")
+s=input("Are you senior citizen?(y/n)")
+
+if g=='female' and s=='y':
+    r=8
+elif g == 'female' and s == 'n':
+    r=6
+elif g == 'male' and s=='y':
+    r=7
+elif g == 'male' and s == 'n':
+    r=5
+else:
+    print("Invalid Input")
+
+simpleInterest=(p*t*r)/100
+print("Simple Intrest is :",simpleInterest)
